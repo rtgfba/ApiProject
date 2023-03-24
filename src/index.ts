70416fbf0fb0e78164bb06bb1d17aa0a81fe4cb2
rtg <120173063+rtgfba@user.nonreply.github.com>
2023-03-24 03:36:01 -0400

    Set up MongoDB

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,11 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
 
 app.use(cors({
     credentials:true,
@@ -18,6 +21,15 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log('Server running on http:://localhost:8080/');
+server.listen(3000, () => {
+    console.log('Server running on http:://localhost:3000/');
 });
+
+const MONGO_URL = process.env.MONGO_URL;
+
+//initiate mongoose 
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+
+//create error catch in the event an error occurs
+mongoose.connection.on('error', (error:Error) => console.log(error));
