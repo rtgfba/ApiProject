b88c32e798900af5c805b3d170fb54a65a01a67d
rtg <120173063+rtgfba@user.nonreply.github.com>
2023-03-24 15:48:04 -0400

    Create login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,44 @@ import express from 'express';
 import {getUserByEmail, createUser} from '../db/users';
 import {random, authentication} from '../helpers';
 
+//create a login controller
+
+export const login = async (req:express.Request, res:express.Response) => {
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+        if(!user){
+            return res.sendStatus(400);
+
+        }
+
+        //authenticate user without knowing their password using hash comparissions 
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if (user.authentication.password != expectedHash){
+            return res.sendStatus(403);
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+        await user.save();
+
+        res.cookie('REMI-AUTH', user.authentication.sessionToken, {domain:'localhost', path: '/'});
+
+        return res.status(200).json(user).end();
+    }catch(error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
 
 //create a register controller
 export const register = async (req:express.Request, res: express.Response) => {
