091587ec760731c113cd006baeba2e92846a5927
rtg <120173063+rtgfba@user.nonreply.github.com>
2023-03-24 23:27:56 -0400

    Create user authentication

---
+++ src/router/users.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import {getAllUsers, deleteUsers} from '../controllers/users';
+import { isAuthenticated, isOwner } from '../middlewares';
+
+
+
+
+export default (router:express.Router) => {
+    router.get('/users', isAuthenticated,getAllUsers);
+    router.delete('/users/:id', isAuthenticated, isOwner, deleteUsers);
+};
+
+
+
